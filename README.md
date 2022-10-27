48649824650b01d063c3d16b0b88de710c7dec4f
patricklai46 <patrick_0406@outlook.com>
2022-10-27 13:41:57 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # externalSort
+
+**Run**
+
+```shell
+$ ./bin/go_build_externalSort_src_linux
+```
+
+**Usage**
+
+```shell
+$ ./bin/go_build_externalSort_src_linux -h
+Usage of ./bin/go_build_externalSort_src_linux:
+  -f string
+        the filename of the data file (default "data")
+  -g    whether generate data
+  -n int
+        the number of the numbers (default 10000000)
+  -t int
+        the number of the threads (default 16)
+```
+
--- src/main.go
+++ src/main.go
@@ -12,11 +12,14 @@ var (
 	fileName = flag.String("f", "data", "the filename of the data file")
 	threads  = flag.Int("t", 16, "the number of the threads")
 	count    = flag.Int("n", 10000000, "the number of the numbers")
+	genData  = flag.Bool("g", false, "whether generate data")
 )
 
 func main() {
 	flag.Parse()
-	generateData(*fileName, *count)          // 随机生成数据
+	if *genData {
+		generateData(*fileName, *count) // 随机生成数据
+	}
 	tmpFile := readData(*fileName, *threads) // 分块进行排序
 	mergeData2(tmpFile, *threads)            // 进行多路合并
 }
