620de251f702ab4cb6b9101bb16d1b1ec0b676d1
patricklai46 <patrick_0406@outlook.com>
2022-10-27 13:43:02 +0800

    update README.md

--- README.md
+++ README.md
@@ -13,9 +13,9 @@ $ ./bin/go_build_externalSort_src_linux -h
 Usage of ./bin/go_build_externalSort_src_linux:
   -f string
         the filename of the data file (default "data")
-  -g    whether generate data
+  -g    whether generate random data
   -n int
-        the number of the numbers (default 10000000)
+        the number of the numbers to be generated (default 10000000)
   -t int
         the number of the threads (default 16)
 ```
--- src/main.go
+++ src/main.go
@@ -11,8 +11,8 @@ import (
 var (
 	fileName = flag.String("f", "data", "the filename of the data file")
 	threads  = flag.Int("t", 16, "the number of the threads")
-	count    = flag.Int("n", 10000000, "the number of the numbers")
-	genData  = flag.Bool("g", false, "whether generate data")
+	count    = flag.Int("n", 10000000, "the number of the numbers to be generated")
+	genData  = flag.Bool("g", false, "whether generate random data")
 )
 
 func main() {
